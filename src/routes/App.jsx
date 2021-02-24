afc21ccd8f9f663d43528c7aaee284beb85ba483
Juan Camilo Rico <juancarico92@gmail.com>
2021-02-23 22:19:36 -0500

    feat: react-router implemented

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+
 import Home from '../pages/Home'
 
 const App = () => {
   return (
-    <>
-      <Home />
-    </>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Home} />
+      </Switch>
+    </Router>
   )
 }
 
