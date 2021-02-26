7af87bc7af3fa66b399eb199c9459bd584d0e1b0
Juan Camilo Rico <juancarico92@gmail.com>
2021-02-26 17:03:08 -0500

    feat(SearchBar): styles implemented; _mixins.scss file created

--- src/layout/components/SearchBar/index.jsx
+++ src/layout/components/SearchBar/index.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
+import './styles.scss'
+import Logo from '../../../assets/img/Logo_ML.png'
+import Search from '../../../assets/img/ic_Search.png'
+
 const SearchBar = () => {
   return (
-    <div>
-      Search Bar
+    <div className='searchbar'>
+      <div className='searchbar__logo'>
+        <img src={Logo} alt='Mercado Libre logo' />
+      </div>
+      <form>
+        <input type='text' placeholder='Nunca dejes de buscar' />
+        <button type='submit'>
+          <img src={Search} alt='search icon' />
+        </button>
+      </form>
     </div>
   )
 }
