187eb0d582b2d31980123cac3487379bb658d5ad
Juan Camilo Rico <juancarico92@gmail.com>
2021-02-27 05:59:00 -0500

    feat(NotFound): content updated and styles added

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
+import { Link, useLocation } from 'react-router-dom'
+import './styles.scss'
+
 const NotFound = () => {
+  const location = useLocation()
+
   return (
     <div className='not-found'>
-      404: Not Found
+      <div className='not-found__title'>404: P&aacute;gina no encontrada</div>
+      <div className='not-found__no-match'>
+        No hay rutas que coincidan con: <code>{location.pathname}</code>
+      </div>
+      <div className='not-found__subtitle'>
+        Intenta llendo a la{' '}
+        <Link className='link' to='/'>
+          Página Principal
+        </Link>
+      </div>
     </div>
   )
 }
