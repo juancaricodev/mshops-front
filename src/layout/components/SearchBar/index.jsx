7a25d20ea2399eb2afb261d3e8d6c6614ac08f08
Juan Camilo Rico <juancarico92@gmail.com>
2021-02-27 02:15:45 -0500

    feat(SearchBar): handlers and local state for input added

--- src/layout/components/SearchBar/index.jsx
+++ src/layout/components/SearchBar/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { Link } from 'react-router-dom'
 
@@ -7,14 +7,31 @@ import Logo from '@img/Logo_ML.png'
 import Search from '@img/ic_Search.png'
 
 const SearchBar = () => {
+  const [search, setSearch] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('Searching...', search)
+  }
+
+  const searchChange = (e) => {
+    setSearch(e.target.value)
+  }
+
   return (
     <div className='searchbar'>
       <div className='searchbar-container'>
         <Link to='/' className='searchbar__logo'>
           <img src={Logo} alt='Mercado Libre logo' />
         </Link>
-        <form>
-          <input type='text' placeholder='Nunca dejes de buscar' />
+
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            placeholder='Nunca dejes de buscar'
+            onChange={searchChange}
+            value={search}
+          />
           <button type='submit'>
             <img src={Search} alt='search icon' />
           </button>
