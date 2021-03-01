c1765d876c05bf60b38a540a3b60b74a550d44d8
Juan Camilo Rico <juancarico92@gmail.com>
2021-03-01 11:06:45 -0500

    feat(NoMatch): template created

--- src/pages/Products/components/NoMatch/index.jsx
+++ src/pages/Products/components/NoMatch/index.jsx
@@ -5,7 +5,11 @@ import './styles.scss'
 const NoMatch = () => {
   return (
     <div className='no-match'>
-      No Match
+      <h2>No hay publicaciones que coincidan con tu b&uacute;squeda.</h2>
+      <ul>
+        <li><strong>Revisa la ortograf&iacute;a</strong> de la palabra.</li>
+        <li>Utiliza <strong>palabras m&aacute;s gen&eacute;ricas</strong> o menos palabras.</li>
+      </ul>
     </div>
   )
 }
