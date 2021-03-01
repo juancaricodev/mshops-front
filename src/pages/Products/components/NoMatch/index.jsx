c846eb23fbbb1989aafe6224c8f5e9fed0619287
Juan Camilo Rico <juancarico92@gmail.com>
2021-03-01 11:01:42 -0500

    feat(NoMatch): component created to be rendered from Products page

---
+++ src/pages/Products/components/NoMatch/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './styles.scss'
+
+const NoMatch = () => {
+  return (
+    <div className='no-match'>
+      No Match
+    </div>
+  )
+}
+
+export default NoMatch
