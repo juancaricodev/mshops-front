8d7a01fb86c1f815ef752417c27431af2d8d3f01
Juan Camilo Rico <juancarico92@gmail.com>
2021-03-01 22:00:53 -0500

    feat(Products): Breadcrumb component implemented

--- src/pages/ProductDetail/index.jsx
+++ src/pages/ProductDetail/index.jsx
@@ -5,6 +5,7 @@ import './styles.scss'
 import detailService from '@services/detail'
 import Spinner from '@utils/components/Spinner'
 import { formatAmount, formatDecimals } from '@utils/functions/formatNumbers'
+import Breadcrumb from '@utils/components/Breadcrumb'
 
 const ProductDetail = () => {
   const [product, setProduct] = useState({})
@@ -36,40 +37,43 @@ const ProductDetail = () => {
   }, [product])
 
   return (
-    <div className='detail'>
-      {loading
-        ? <Spinner />
-        : (
-          <>
-            <section className='detail__cover'>
-              <img src={product.picture} alt={product.title} />
-            </section>
+    <>
+      <Breadcrumb />
+      <div className='detail'>
+        {loading
+          ? <Spinner />
+          : (
+            <>
+              <section className='detail__cover'>
+                <img src={product.picture} alt={product.title} />
+              </section>
 
-            <section className='detail__info'>
-              <div className='detail__info-status'>
-                <span>{product.condition}</span> - {product.sold_quantity} vendidos
-              </div>
+              <section className='detail__info'>
+                <div className='detail__info-status'>
+                  <span>{product.condition}</span> - {product.sold_quantity} vendidos
+                </div>
 
-              <div className='detail__info-title'>
-                {product.title}
-              </div>
+                <div className='detail__info-title'>
+                  {product.title}
+                </div>
 
-              <div className='detail__info-price'>
-                &#36; {amount}
-                <sup>{decimals < 10 ? `0${decimals}` : decimals}</sup>
-              </div>
+                <div className='detail__info-price'>
+                  &#36; {amount}
+                  <sup>{decimals < 10 ? `0${decimals}` : decimals}</sup>
+                </div>
 
-              <button type='button'>Comprar</button>
-            </section>
+                <button type='button'>Comprar</button>
+              </section>
 
-            <section className='detail__description'>
-              <div className='detail__description-title'>Descipci&oacute;n del producto</div>
+              <section className='detail__description'>
+                <div className='detail__description-title'>Descipci&oacute;n del producto</div>
 
-              <div className='detail__description-content'>{product.description}</div>
-            </section>
-          </>
-          )}
-    </div>
+                <div className='detail__description-content'>{product.description}</div>
+              </section>
+            </>
+            )}
+      </div>
+    </>
   )
 }
 
