c5673e201bb1490567bea2a3949c499d4b2fd995
Juan Camilo Rico <juancarico92@gmail.com>
2021-03-03 12:25:20 -0500

    feat(copy-webpack-plugin): dependency installed and webpack plugin configured

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
@@ -76,6 +77,20 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
       filename: 'assets/[name].css'
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: './public/robots.txt',
+          to: '/',
+          toType: 'dir'
+        },
+        {
+          from: './public/sitemap.xml',
+          to: '/',
+          toType: 'dir'
+        }
+      ]
     })
   ],
   devServer: {
