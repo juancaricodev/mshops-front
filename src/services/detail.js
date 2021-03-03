1d0daa697e629a7aadeb5576975e79132fa84bb9
Juan Camilo Rico <juancarico92@gmail.com>
2021-03-03 18:37:31 -0500

    docs(services/): task description comments added

--- src/services/detail.js
+++ src/services/detail.js
@@ -1,8 +1,13 @@
 const URL = 'https://polar-tundra-66918.herokuapp.com/api/items'
 
+// The function gets the product's id
 const getId = (id) => {
+  // Concatenate the URL and the product id for fetching its data
   const data = fetch(`${URL}/${id}`)
+
+  // Transform response into JSON format
   const response = data.then(res => res.json())
+
   return response
 }
 
