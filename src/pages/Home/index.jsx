6d031f71ed86862e8a9e73f3ec317c057c141dcb
Juan Camilo Rico <juancarico92@gmail.com>
2021-03-02 21:52:33 -0500

    feat(Home): react-helmet implemented

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
+import { Helmet } from 'react-helmet'
 import './styles.scss'
 
 const Home = () => {
   return (
     <div className='home'>
+      <Helmet>
+        <title>Mercado Libre - by: juancaricodev</title>
+        <meta name='description' content='La comunidad de compra y venta online más grande de América Latina.' />
+      </Helmet>
+
       <p>
         ¡Bienvenidos a Mercado Libre!
       </p>
