04a99cab4c1d001030ff577fca927377b666ba1d
Juan Camilo Rico <juancarico92@gmail.com>
2021-03-03 19:13:34 -0500

    docs(utils/functions/): task description comments added

--- src/utils/functions/formatAccents.js
+++ src/utils/functions/formatAccents.js
@@ -1,7 +1,9 @@
-const format = (string) => {
+// Function receives a string with accent marks to be erased
+const formatAccents = (string) => {
+  // Separate the accent marks from the letters and they're deleted
   const formatted = string.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
 
   return formatted
 }
 
-export default format
+export default formatAccents
--- src/utils/functions/formatNumbers.js
+++ src/utils/functions/formatNumbers.js
@@ -1,10 +1,18 @@
+// Function receives a number as param. to be formatted into a string = [000.000]
 export const formatAmount = (value) => {
+  // RexEx for dividing the param. received
   const divider = /\B(?=(\d{3})+(?!\d))/g
+
+  // Transform the param. into a string and apply the divider
   const valueFormatted = value.toString().replace(divider, '.')
+
   return valueFormatted
 }
 
+// Function receives a number (decimals) as param. to be transformed into format = [00]
 export const formatDecimals = (value) => {
+  // Param. rounded and multiplied by 100 for formatting into [00]
   const valueFormatted = Math.round(value * 100)
+
   return valueFormatted
 }
