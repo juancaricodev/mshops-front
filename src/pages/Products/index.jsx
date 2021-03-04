9d1191c49126b5d56007ad2a15e1d94ec05f7042
Juan Camilo Rico <juancarico92@gmail.com>
2021-03-03 20:21:05 -0500

    docs(Products): description comments added

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -7,23 +7,36 @@ import './styles.scss'
 import ShippingIcon from '@img/ic_shipping.png'
 import Product from './components/Product'
 import productsService from '@services/products'
-import format from '@utils/functions/formatAccents'
+import formatAccents from '@utils/functions/formatAccents'
 import capitalizeString from '@utils/functions/capitalizeString'
 import Spinner from '@utils/components/Spinner'
 import Breadcrumb from '@utils/components/Breadcrumb'
 
 const NoMatch = lazy(() => import('./components/NoMatch'))
 
+/**
+ * Products
+ * Page rendered when a product is searched through the SearchBar component
+ * Implements lazy loading for rendering NoMatch component only when needed
+ */
 const Products = () => {
+  // State implemented for storing the products once fetched
   const [products, setProducts] = useState({})
+  // Raw query, needs to be formatted erasing the accent marks
   const [query, setQuery] = useState()
+  // Query formatted (accent marks deleted)
   const [queryFormat, setQueryFormat] = useState()
+  // loading state set true when data hasn't been retrieved yet
   const [loading, setLoading] = useState(true)
+  // noMatch state set true when query doesn't match any result
   const [noMatch, setNoMatch] = useState(false)
 
+  // { useLocation, useHistory } -> react-router-dom hooks for interacting with url state
   const querySearch = new URLSearchParams(useLocation().search)
   const history = useHistory()
 
+  // Side effect watching on querySearch (useLocation hook)
+  // Gets 'search' query param. and sets it to be formatted
   useEffect(() => {
     const queryString = querySearch.get('search')
 
@@ -31,12 +44,14 @@ const Products = () => {
       ? setQuery(queryString)
       : history.push('/')
 
-    query && Promise.resolve(format(query))
+    query && Promise.resolve(formatAccents(query))
       .then(res => setQueryFormat(res))
 
     return () => setQuery()
   }, [querySearch])
 
+  // Side effect watching on queryFormat
+  // Once query is formatted, is executed for fetching products data
   useEffect(() => {
     queryFormat && productsService
       .getAll(queryFormat)
@@ -44,6 +59,8 @@ const Products = () => {
       .catch(err => console.error(err))
   }, [queryFormat])
 
+  // Side effect watching on products state
+  // Handlign loading state and noMatch state
   useEffect(() => {
     Object.keys(products).length > 0 && setLoading(false)
 
